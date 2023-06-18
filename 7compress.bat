38b7f0ccaae74158e2e944b28276c6748a8d7800
Rzv1337 <80308384+Rzv1337@users.noreply.github.com>
2023-06-18 19:01:35 +0300

    Update 7compress.bat

--- 7compress.bat
+++ 7compress.bat
@@ -1,65 +1,11 @@
 @echo off
-for /r %%v in (*.nav) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.bsp) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.mp3) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.wav) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.kv) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.txt) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.dds) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.png) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.jpg) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.svg) do (
-  7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.vtx) do (
-   7za a "%%~dpnv%%~xv.bz2" "%%v"
-   del "%%v"
-)
-for /r %%v in (*.vvd) do (
-   7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.phy) do (
-   7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.mdl) do (
-   7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.vmt) do (
-   7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
-)
-for /r %%v in (*.vtf) do (
-   7za a "%%~dpnv%%~xv.bz2" "%%v"
-  del "%%v"
+for /r %%v in (*) do (
+    if /I not "%%~nxv"=="7compress.bat" (
+        if /I not "%%~nxv"=="7za.dll" (
+            if /I not "%%~nxv"=="7za.exe" (
+                7za a "%%~dpnv%%~xv.bz2" "%%v"
+                del "%%v"
+            )
+        )
+    )
 )
