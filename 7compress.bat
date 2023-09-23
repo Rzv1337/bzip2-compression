8be0d8fa9a2a8a67cc930d2d1bc75be69f0e4308
Rzv1337 <80308384+Rzv1337@users.noreply.github.com>
2023-09-23 13:26:12 +0300

    Update 7compress.bat

--- 7compress.bat
+++ 7compress.bat
@@ -3,8 +3,12 @@ for /r %%v in (*) do (
     if /I not "%%~nxv"=="7compress.bat" (
         if /I not "%%~nxv"=="7za.dll" (
             if /I not "%%~nxv"=="7za.exe" (
-                7za a "%%~dpnv%%~xv.bz2" "%%v"
-                del "%%v"
+                if /I "%%~xv"==".bz2" (
+                    echo Skipping .bz2 file "%%v"
+                ) else (
+                    7za a "%%~dpnv%%~xv.bz2" "%%v"
+                    del "%%v"
+                )
             )
         )
     )
